1d3475c3b827e68ff745c893f3a84e4cc4e18231
luckypeppa <871565198@qq.com>
2022-05-11 14:40:39 +0800

    Set up Register page.

---
+++ src/views/user/Register.vue
@@ -0,0 +1,18 @@
+<template>
+  <div></div>
+</template>
+
+<script>
+export default {
+  name: "userRegister",
+  data() {
+    return {
+      email: "",
+      username: "",
+      password: "",
+    };
+  },
+};
+</script>
+
+<style lang="scss" scoped></style>
