9b68a18d0f40b7a2c0cdeea9e7855237490092cf
luckypeppa <871565198@qq.com>
2022-05-16 11:14:46 +0800

    Add input for title and tag

--- src/views/post/PostCreate.vue
+++ src/views/post/PostCreate.vue
@@ -1,5 +1,9 @@
 <template>
   <div class="container">
+    <div class="info">
+      <BaseInput type="text" label="Title" v-model="title" />
+      <BaseInput type="text" label="Tag" v-model="tag" />
+    </div>
     <tip-tap v-model="content" />
   </div>
 </template>
@@ -10,6 +14,8 @@ export default {
   data() {
     return {
       content: "",
+      title: "",
+      tag: "",
     };
   },
   components: {
@@ -18,4 +24,10 @@ export default {
 };
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.info {
+  display: flex;
+  justify-content: space-between;
+  margin-bottom: 1rem;
+}
+</style>
