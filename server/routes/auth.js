bffd7d4b641a3033c788b13a227fb80693bf573e
luckypeppa <871565198@qq.com>
2022-06-21 00:05:18 +0800

    Add user and refreshToken schema.

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -3,82 +3,119 @@ const express = require("express");
 const jwt = require("jsonwebtoken");
 const bcrypt = require("bcrypt");
 
+const User = require("../model/user");
+const RefreshToken = require("../model/refreshToken");
+
 const router = express.Router();
 
-const refreshTokens = [];
-const users = [];
-
 // register user
-router.post("/register", async (req, res) => {
+router.post("/register", (req, res) => {
   const { username, password } = req.body;
   if (!username || !password) return res.sendStatus(400);
-  try {
-    const hashedPassword = await bcrypt.hash(password, 10);
-    users.push({
-      username,
-      hashedPassword,
+
+  User.findOne({ username: username })
+    .then((existedUser) => {
+      if (existedUser) return res.sendStatus(400);
+      return bcrypt.hash(password, 10);
+    })
+    .then((hashedPassword) => {
+      const user = new User({
+        username,
+        hashedPassword,
+      });
+      return user.save();
+    })
+    .then(() => res.sendStatus(201))
+    .catch((err) => {
+      console.log("err:", err);
+      res.sendStatus(500);
     });
-    res.sendStatus(201);
-  } catch {
-    res.sendStatus(500);
-  }
 });
 
-router.post("/login", async (req, res) => {
+// user login
+router.post("/login", (req, res) => {
   const { username, password } = req.body;
   if (!username || !password) return res.sendStatus(400);
 
-  try {
-    const existedUser = users.filter((user) => user.username === username)[0];
-    if (!existedUser) return res.sendStatus(404);
-    const result = await bcrypt.compare(password, existedUser.hashedPassword);
-    if (!result) return res.sendStatus(403);
-  } catch {
-    return res.sendStatus(500);
-  }
+  // find existed user
+  User.findOne({ username: username })
+    .then((existedUser) => {
+      console.log(existedUser);
+      if (!existedUser) return res.sendStatus(404);
+      // compare passwords
+      return bcrypt.compare(password, existedUser.hashedPassword);
+    })
+    .then((result) => {
+      if (!result) return res.sendStatus(403);
+      const user = { username: username };
 
-  const user = { username: username };
+      // generate access token and refresh token
+      const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+        expiresIn: "15s",
+      });
+      const refreshToken = jwt.sign(user, process.env.REFRESH_TOKEN_SECRET);
+      const newRefreshToken = new RefreshToken({
+        token: refreshToken,
+      });
 
-  const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
-    expiresIn: "15s",
-  });
-  const refreshToken = jwt.sign(user, process.env.REFRESH_TOKEN_SECRET);
-  refreshTokens.push(refreshToken);
-
-  res.json({
-    accessToken,
-    refreshToken,
-  });
+      //   return newRefreshToken.save();
+      return new Promise((resolve, reject) => {
+        newRefreshToken
+          .save()
+          .then(() => resolve({ accessToken, refreshToken }))
+          .catch((err) => reject(err));
+      });
+    })
+    .then(({ accessToken, refreshToken }) =>
+      // send back tokens
+      res.json({
+        accessToken,
+        refreshToken,
+      })
+    )
+    .catch((err) => {
+      console.log("err:", err);
+      return res.sendStatus(500);
+    });
 });
 
 // regenerate access token
 router.post("/token", (req, res) => {
   const refreshToken = req.body.refreshToken;
-  console.log(refreshToken);
 
   if (!refreshToken) return res.sendStatus(401);
 
-  if (!refreshTokens.includes(refreshToken)) return res.sendStatus(403);
-
-  jwt.verify(refreshToken, process.env.REFRESH_TOKEN_SECRET, (err, user) => {
-    console.log("err:", err);
-    if (err) return res.sendStatus(403);
-    const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
-      expiresIn: "15s",
+  RefreshToken.findOne({ token: refreshToken })
+    .then((token) => {
+      if (!token) return res.sendStatus(403);
+      jwt.verify(
+        refreshToken,
+        process.env.REFRESH_TOKEN_SECRET,
+        (err, user) => {
+          console.log("err:", err);
+          if (err) return res.sendStatus(403);
+          const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+            expiresIn: "15s",
+          });
+          res.json({ accessToken });
+        }
+      );
+    })
+    .catch((err) => {
+      console.log("err:", err);
+      return res.sendStatus(500);
     });
-    res.json({ accessToken });
-  });
 });
 
-router.get("/post", (req, res) => {
-  const authHeader = req.headers["authorization"];
-  const accessToken = authHeader.split(" ")[1];
-  if (!accessToken) return res.sendStatus(401);
-  jwt.verify(accessToken, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
-    console.log("err:", err);
-    if (err) return res.sendStatus(403);
-    res.json({ msg: "got" });
-  });
-});
+// router.get("/post", (req, res) => {
+//   const authHeader = req.headers["authorization"];
+//   const accessToken = authHeader.split(" ")[1];
+//   if (!accessToken) return res.sendStatus(401);
+//   jwt.verify(accessToken, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
+//     console.log("err:", err);
+//     if (err) return res.sendStatus(403);
+//     res.json({ msg: "got" });
+//   });
+// });
 
 module.exports = router;
