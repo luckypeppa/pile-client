4971914e45d76666b42989d78d30461364922539
luckypeppa <871565198@qq.com>
2022-06-22 00:37:02 +0800

    Ajust css for PostDetail

--- src/views/post/PostDetail.vue
+++ src/views/post/PostDetail.vue
@@ -1,15 +1,16 @@
 <template>
   <div class="detail-container">
-    <div class="cover-container">
+    <div class="time-container">
+      <time v-if="post.createdAt" class="time">{{
+        new Date(post.createdAt).toString()
+      }}</time>
       <div class="tag">{{ post.tag.name }}</div>
+    </div>
+    <div class="cover-container">
       <img :src="post.coverUrl" alt="" class="cover" />
     </div>
     <div class="detail">
       <h1 class="title">{{ post.title }}</h1>
-      <p class="tag">{{ post.tag.name }}</p>
-      <time v-if="post.createdAt" class="time">{{
-        new Date(post.createdAt).toString()
-      }}</time>
       <div class="content" ref="body"></div>
     </div>
   </div>
@@ -33,40 +34,50 @@ export default {
 
 <style lang="scss" scoped>
 .detail-container {
-  .cover-container {
-    position: relative;
-    .tag {
-      position: absolute;
-      left: 2rem;
-      top: 2rem;
-      background-color: lightblue;
-      padding: 1rem 2rem;
-      border-radius: 2rem;
-      box-shadow: 0 0 3px 3px rgba($color: #000000, $alpha: 0.1);
-    }
-    .cover {
-      width: 100%;
-      aspect-ratio: 16 / 9;
-      object-fit: cover;
-      box-shadow: 0 3px 3px 3px rgba($color: #000000, $alpha: 0.1);
-    }
-  }
-  .detail {
-    box-shadow: 0 0 3px 1px rgba($color: #000000, $alpha: 0.1);
-    padding: 1rem;
-    transform: translateY(-20rem);
-    background-color: white;
-    width: min(60rem, 100%);
-    margin: auto;
-    .title,
-    .tag {
-      margin-bottom: 1rem;
-    }
+  .time-container {
+    display: flex;
+    gap: 2rem;
+    align-items: center;
+    margin-bottom: 1rem;
 
     .time {
+      color: grey;
       display: block;
-      margin-bottom: 2rem;
     }
+
+    .tag {
+      color: grey;
+      position: relative;
+
+      &::before {
+        content: "";
+        width: 0.3rem;
+        height: 0.3rem;
+        background-color: red;
+        border-radius: 50%;
+        position: absolute;
+        left: -1rem;
+        top: 50%;
+        transform: translateY(-50%);
+      }
+    }
+  }
+  .cover {
+    display: block;
+    width: min(100%, 40rem);
+    aspect-ratio: 2 / 1;
+    object-fit: cover;
+    margin: 2rem auto;
+  }
+}
+.detail {
+  padding: 1rem;
+  width: min(60rem, 100%);
+  margin: auto;
+  .title {
+    margin-bottom: 2rem;
+    text-align: center;
+    font-size: clamp(2rem, 7vw, 5rem);
   }
 }
 </style>
